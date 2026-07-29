88fb3d3e671a0bfdb5f856362ba3080177fd8c9d
lww209 <khooliwern@gmail.com>
2026-07-29 10:50:40 +0800

    swd

--- Final.py
+++ Final.py
@@ -4,6 +4,8 @@ POST_FILE = "posts.txt"
 ENGAGEMENT_FILE = "engagement.txt"
 # REPORT_FILE = "report.txt"
 
+
+
 # -----Exit the program gracefully with a goodbye message -----
 def exit_program():
     print("\nThanks for using the Social Media Planner. Goodbye!")
@@ -13,15 +15,14 @@ def exit_program():
 original_input = input
 
 # Safe input function
-def safe_input(prompt): # Prompt = the text you pass when asking the user for input
-    user_input = original_input(prompt) # Whatever the user types will be stored in 'user_input'
-    if user_input.lower() in ["cancel", "exit", "end"]: # Converts input to lowercase
+def safe_input(prompt): # prompt = the text you pass when asking the user for input
+    user_input = original_input(prompt)  # whatever the user types will be stored in 'user_input'
+    if user_input.lower() in ["cancel", "exit", "end"]: #converts input to lowercase
         exit_program()
     return user_input
 
 # Override input globally
 input = safe_input # Replaces Python's built-in 'input' function with safe_input
-
 
 
 
@@ -37,9 +38,7 @@ def display_menu():
     print("4. Display Content Calendar")
     print("5. Generate Performance Report")
     print("6. Export Report to File")
-    print("7. Exit") # Change to "Press X/type [keyword] (ex.exit) to exit the programm at any given moment"
-
-
+    print("7. Exit")# change to "Press X/type [keyword] (ex.exit) to exit the programm at any given moment"
 
 
 # ----- Add a new post to the posts.txt file -----
@@ -209,7 +208,7 @@ def display_content_calendar():
 
         post_id = fields[0]
         platform = fields[1]
-        date = fields[3].replace("-", "/") # Changes DD-MM-YYYY to DD/MM/YYYY
+        date = fields[3].replace("-", "/")   # Changes DD-MM-YYYY to DD/MM/YYYY
         status = fields[4]
 
         print(f"{post_id:<10}{date:<13}{platform:<15}{status}")
@@ -381,7 +380,7 @@ def main():
         elif choice == "6":
             results = generate_performance_report()
             if results:
-                export_report(*results) # Unpack the results and pass them to export_report
+                export_report(*results)  # Unpack the results and pass them to export_report
         elif choice == "7":
             exit_program()
             break
